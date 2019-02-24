ceaed64c72f376f97a3c25811635796241026796
Horsuna <a.tahir4@wlv.ac.uk>
2019-02-24 21:49:40 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,8 @@ echo "Downloading RocketMod..."
 curl -o Rocket.zip "https://ci.rocketmod.net/job/Rocket.Unturned/lastSuccessfulBuild/artifact/Rocket.Unturned/bin/Release/Rocket.zip"
 unzip -o -q Rocket.zip
 mv /home/container/Scripts/Linux/RocketLauncher.exe /home/container/RocketLauncher.exe
+mv /home/container/Scripts/Linux/start.sh /home/container/start.sh
+mv /home/container/Scripts/Linux/update.sh /home/container/update.sh
 
 # Feature removed from panel?
 #if [ -z "${ALLOC_0__PORT}" ] || [ "$((ALLOC_0__PORT-1))" != "${SERVER_PORT}" ]; then
